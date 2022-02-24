a3800e11990b1c08c257012b02c0919bd12ec794
krisgrm <kristjan-ext@allianceblock.io>
2022-02-24 10:07:21 +0100

    updated CMS contract

--- contracts/CMS.sol
+++ contracts/CMS.sol
@@ -8,8 +8,10 @@ contract CMS {
         bytes data
     );
 
-    function stateChange(bytes calldata data_) external {
-        emit StateChange(msg.sender, data_);
+    function stateChange(bytes[] calldata data_) external {
+        for (uint256 i_ = 0; i_ < data_.length; i_++) {
+            emit StateChange(msg.sender, data_[i_]);
+        }
     }
 
 }
